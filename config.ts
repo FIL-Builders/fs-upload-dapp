13f99282202c7c58cd383475aa635eebc52ec66a
nijoe1 <nick@fil.builders>
2025-09-24 13:16:19 +0400

    improvements

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@
 
 export const config = {
   // The number of GB of storage capacity needed to be sufficient
-  storageCapacity: 100,
+  storageCapacity: 10,
   // The number of days of lockup needed to be sufficient
   persistencePeriod: 30,
   // The minimum number of days of lockup needed to be sufficient
